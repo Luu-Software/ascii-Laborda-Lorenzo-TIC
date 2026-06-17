edfdc10e967cc6b1382b70f14fae145302b8b1e2
Laborda-Lorenzo-TIC <lorenzofernandolaborda@gmail.com>
2026-06-17 15:48:34 -0300

    consigna completada

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
+  return (letra.repeat(cantidad))
   // COMPLETAR
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
-
 // COMPLETAR
+let filas:number = tamaño
+
+while (filas > 0){
+  console.log (repetirLetra(caracter, tamaño))
+  filas = filas - 1
+}
